99a04766059193ae35a844fd55e0cd4b85085f0d
gyanendrav2 <gyanendrav2@gmail.com>
2021-03-31 19:15:20 +0530

    added the loader

---
+++ src/components/loader/Loader.js
@@ -0,0 +1,29 @@
+import { Box, makeStyles } from "@material-ui/core"
+import React from "react"
+import { icons } from "../../assets/icons"
+import { colors } from "../../theme/colors"
+
+const useStyles = makeStyles({
+    wrapper: {
+        backgroundColor: colors.black,
+        width: "100%",
+        height: "100vh",
+        zIndex: 9,
+        position: "fixed",
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+    },
+    image: {
+        animation: "zoomIn 1s linear infinite",
+    },
+})
+
+export const Loader = () => {
+    const classes = useStyles()
+    return (
+        <Box className={classes.wrapper}>
+            <img src={icons.logoWhite} className={classes.image} alt="logo" />
+        </Box>
+    )
+}
--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,4 +1,4 @@
-import React from "react"
+import React, { useState } from "react"
 import PropTypes from "prop-types"
 import Head from "next/head"
 import { ThemeProvider } from "@material-ui/core/styles"
@@ -7,16 +7,24 @@ import theme from "../theme/theme"
 import { images } from "../assets/images"
 import "react-lazy-load-image-component/src/effects/blur.css"
 import "../styles/index.scss"
+import { Loader } from "../components/loader/Loader"
 
 export default function MyApp(props) {
     const { Component, pageProps } = props
+    const [isLoading, setIsLoading] = useState(true)
 
     React.useEffect(() => {
-        /* Remove the server-side injected CSS. */
+        const loaderhander = () => {
+            setIsLoading(false)
+            // document.getElementById("__next").style.display = "block"
+        }
+        // document.getElementById("__next").style.display = "none"
+        window.addEventListener("load", loaderhander)
         const jssStyles = document.querySelector("#jss-server-side")
         if (jssStyles) {
             jssStyles.parentElement.removeChild(jssStyles)
         }
+        return () => window.removeEventListener("load", loaderhander)
     }, [])
 
     return (
@@ -27,6 +35,8 @@ export default function MyApp(props) {
                 <meta name="viewport" content="minimum-scale=1, initial-scale=1, width=device-width" />
                 <meta name="description" content="Web site created using next js" />
             </Head>
+            {isLoading && <Loader />}
+
             <ThemeProvider theme={theme}>
                 {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
                 <CssBaseline />
